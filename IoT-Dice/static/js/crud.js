46628679e7d4c21355540ba045c108e63609c1a7
S. Karpuz <selin.karpuz@hva.nl>
2021-10-28 00:23:05 +0200

    [#9] Add CREATE function to crud.js to store entry in DB + file formatting

--- IoT-Dice/static/js/crud.js
+++ IoT-Dice/static/js/crud.js
@@ -1,41 +1,79 @@
 /**
  * This JS file contains logic for the CRUD operations
  */
- const mysqlConnection = require("./mysqlService");
+const mysqlConnection = require("./mysqlService");
 
- /**
+/**
+ * CREATE
+ * Creates a new entry in the database representing a new die roll.
+ */
+function CREATE(numberRolled) {
+    mysqlConnection.getConnection((err, conn) => {
+        if (err) {
+            console.log("ERROR!");
+            throw err;
+        }
+
+        let sql = "INSERT INTO `dicedata`(`number`, `date`, `time`) VALUES (?, ?, ?);";
+
+        let date = new Date();
+        let dateString = date.getFullYear() + "-" + ("0" + date.getMonth()).slice(-2) + "-" + ("0" + date.getDay()).slice(-2);
+
+        let inserts = [
+            numberRolled,
+            dateString,
+            date.toLocaleTimeString(),
+        ];
+
+        console.log(inserts);
+
+        sql = mysqlConnection.formatSQL(sql, inserts);
+
+        conn.query(sql, function (err, rows) {
+            if (err) {
+                throw err;
+            }
+            console.log("Created a new entry in the database!");
+        });
+
+        conn.release();
+    });
+}
+
+/**
  * READ
  * Reads data from the database. In particular for populating the table on the index.html page.
  */
 function READ(callback) {
     mysqlConnection.getConnection((err, conn) => {
-      if (err) {
-        console.log("ERROR!");
-        throw err;
-      }
-  
-      let sql = "SELECT * from dicedata ORDER BY time DESC;";
-  
-      conn.query(sql, function (err, rows) {
         if (err) {
-          throw err;
+            console.log("ERROR!");
+            throw err;
         }
-  
-        let arrayDicedata = [];
-  
-        rows.forEach((row) => {
-          arrayDicedata.push({
-            date: row.date,
-            time: row.time,
-            number: row.number,
-          });
-        });
-  
-        callback(arrayDicedata);
-      });
-  
-      conn.release();
-    });
-  }
 
- exports.getDiceData = READ;
+        let sql = "SELECT * from dicedata ORDER BY time DESC;";
+
+        conn.query(sql, function (err, rows) {
+            if (err) {
+                throw err;
+            }
+
+            let arrayDicedata = [];
+
+            rows.forEach((row) => {
+                arrayDicedata.push({
+                    date: row.date,
+                    time: row.time,
+                    number: row.number,
+                });
+            });
+
+            callback(arrayDicedata);
+        });
+
+        conn.release();
+    });
+}
+
+exports.create = CREATE;
+exports.getDiceData = READ;

19aed5e91bab82fd5d67820e8c8e2629ec345e45
S. Karpuz <selin.karpuz@hva.nl>
2021-10-27 23:24:38 +0200

    [#18] Remove mysql code from dice-server.js

--- IoT-Dice/dice-server.js
+++ IoT-Dice/dice-server.js
@@ -6,21 +6,6 @@ require('dotenv').config({
     path: path.resolve(__dirname, './dice-server.env')
 });
 
-const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.DB_PORT
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.log(err.message);
-    }
-    console.log("DB " + connection.state);
-});
-
 var app = express();
 
 // index.html in '/static' folder

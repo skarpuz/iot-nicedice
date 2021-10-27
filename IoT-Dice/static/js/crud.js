3ef4270bb1530dd765ff3e6deb162ed3f16b3d1a
S. Karpuz <selin.karpuz@hva.nl>
2021-10-27 23:28:14 +0200

    [#18] Add READ function in crud.js to fetch dicedata

---
+++ IoT-Dice/static/js/crud.js
@@ -0,0 +1,41 @@
+/**
+ * This JS file contains logic for the CRUD operations
+ */
+ const mysqlConnection = require("./mysqlService");
+
+ /**
+ * READ
+ * Reads data from the database. In particular for populating the table on the index.html page.
+ */
+function READ(callback) {
+    mysqlConnection.getConnection((err, conn) => {
+      if (err) {
+        console.log("ERROR!");
+        throw err;
+      }
+  
+      let sql = "SELECT * from dicedata ORDER BY time DESC;";
+  
+      conn.query(sql, function (err, rows) {
+        if (err) {
+          throw err;
+        }
+  
+        let arrayDicedata = [];
+  
+        rows.forEach((row) => {
+          arrayDicedata.push({
+            date: row.date,
+            time: row.time,
+            number: row.number,
+          });
+        });
+  
+        callback(arrayDicedata);
+      });
+  
+      conn.release();
+    });
+  }
+
+ exports.create = CREATE;
